f137a258de958f00c97cb1197da053b9acd1ff91
Sabrina Soares <sabrinasoareshwang@gmail.com>
2022-01-07 11:28:35 -0300

    imc

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const calcular = document.getElementById('calcular');
+
+function imc() {
+    const nome = document.getElementById ('nome').value;
+    const altura = document.getElementById ('altura').value;
+    const peso = document.getElementById ('peso').value;
+    const idade = document.getElementById ('idade');
+    const resultado = document.getElementById ('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '' && idade !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.5) {
+            classificacao = 'Abaixo do peso.';
+        } else if (valorIMC < 25) {
+            classificacao = 'com o peso ideal.';
+        } else if (valorIMC < 30) {
+            classificacao = 'acima do peso.';
+        } else if (valorIMC < 35) {
+            classificacao = 'com obesidade grau 1.';
+        } else if (valorIMC < 40) {
+            classificacao = 'com obesidade grau 2.';
+        } else {
+            classificacao = 'com obesidade grau 3, obesidade morbida, Procure um médico!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos!'
+    }
+}
+
+calcular.addEventListener('click', imc);
